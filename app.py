21b8aa461c333e759910c7f8364574b6987e0abf
amina-work <mina.works28@gmail.com>
2023-10-21 17:40:41 +0100

    prog point, split database to training + testing files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from PIL import Image
-#import numpy as np
+import random
+import numpy as np
 import os
 
 # Open a TIF file
@@ -8,12 +9,26 @@ image = Image.open('database\\101_1.tif')
 #importing the folder with the images
 direct = 'C:\\Users\\DrdrA\\OneDrive\\Desktop\\UNI\\DeepLearning\\TP1\\database'
 
+# Create empty lists for training and testing file paths
+training_files = []
+testing_files = []
+
 for fingerprint in os.listdir(direct):
     if fingerprint.endswith('.tif'):
         file_path = os.path.join(direct, fingerprint)
+        #for checking if it's works:
         image = Image.open(file_path)
         print(image)
+        #using random.choices to add to the testing or training files:
+        if random.choices([True, False], [0.8, 0.2])[0]:
+            training_files.append(file_path)
+        else:
+            testing_files.append(file_path)
         #process l'image
 
         #process fini
-        image.close()
+        #image.close()
+
+# Print the number of images in each set
+print(f"Number of images in training set: {len(training_files)}")
+print(f"Number of images in testing set: {len(testing_files)}")
